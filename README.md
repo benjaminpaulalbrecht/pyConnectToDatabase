aff0718cbc20b20034ee33514453b2e069eb4fa2
Albrecht <Benjamin.Albrecht@energyTransfer.com>
2020-11-10 15:32:52 -0600

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Exploration of Python Modules, using connections to database as an example
---
+++ pyConnectToSQL.py
@@ -0,0 +1,29 @@
+# MSSQL to Python 
+
+import pyodbc 
+
+
+# server examples (connectiong stuff) 
+
+#server = 'tcp:myserver.database.windows.net' # obviously, theses are BS values 
+
+
+
+server ='WS3040532\WORBENCH' # for a named instance 
+database = 'ComOps' 
+username = 'sa'
+password = 'sa' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE=+database+';UID='+username=+';PWD='"password') #this may not be correct... 
+cursor = cnxn.cursor() 
+
+
+# to execute a query 
+
+cursor.execute("SELECT @@version;") 
+row = cursor.fetchone() 
+
+# loop through the rowset (basically) 
+while row: 
+    print(row[0]) 
+    row = cursor.fetchone() # fetch another ... welll.... row... 
+
